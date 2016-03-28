2e9f5060c2283d25884301ea4f4c8ba55fca13c7
Kévin Ta <kevin.ta@edu.esiee.fr>
2016-03-28 23:06:22 +0200

    Fixed team select on PlayerType

--- src/CPC/PlayerBundle/Form/PlayerType.php
+++ src/CPC/PlayerBundle/Form/PlayerType.php
@@ -5,19 +5,30 @@ namespace CPC\PlayerBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Doctrine\ORM\EntityRepository;
 
 class PlayerType extends AbstractType
 {
+    protected $videogame;
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $videogame = $this->videogame;
+
         $builder
             ->add('nickname', 'text', array(
-                  'label' => 'Pseudo In Game'))
+                    'label' => 'Pseudo In Game'))
             ->add('team', 'entity', array(
-                  'class' => 'CPCTeamBundle:Team',
-                  'required' => false,
-                  'choice_label' => 'name',
-                  'label' => 'Ton équipe ?'))
+                    'class' => 'CPCTeamBundle:Team',
+                    'required' => false,
+                    'choice_label' => 'name',
+                    'query_builder' => function(EntityRepository $repository) use ($videogame) { 
+                        return $repository->createQueryBuilder('u')
+                                            ->where("u.videogame = :videogame")
+                                            ->setParameter('videogame', $videogame)
+                                            ->orderBy('u.name', 'ASC');
+                    },
+                    'label' => 'Ton équipe ?'))
         ;
     }
 
@@ -27,5 +38,15 @@ class PlayerType extends AbstractType
             'data_class' => 'CPC\PlayerBundle\Entity\Player',
             'csrf_protection' => false,
         ));
+    }
+
+    public function getName()
+    {
+        return 'player';
+    }
+
+    public function __construct ($videogame)
+    {
+        $this->videogame = $videogame;
     }
 }
